97a0b31a4db2d1610284dfa46dd908ac46500626
Alejandro Salas <asalas.wd@outlook.com>
2019-12-01 20:11:09 -0500

    add basic functional components

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>This is an unordered list</h2>
+      <ul>
+        <li>One thing</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+function PersonalInfo(){
+  return(
+    <div>
+      <h2>My name is Alejandro Salas</h2>
+      <p>I am a full-stack Web Developer.</p>
+      <p>These are my goals for 2020:</p>
+      <ul>
+        <li>Get a Web Developer job</li>
+        <li>Travel to Madrid</li>
+        <li>Run a 10k</li>
+      </ul>
+    </div>
+  );
+}
+
+export default PersonalInfo;
