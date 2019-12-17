1a4aef923b2518576ef9c4e680574c13b37724e3
Alejandro Salas <asalas.wd@outlook.com>
2019-12-16 21:15:54 -0500

    import and use NavBar component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
+import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import PersonalInfo from './components/PersonalInfo';
 import * as serviceWorker from './serviceWorker';
@@ -9,6 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
 
   <div>
+  <NavBar />
   <h1>Below we have a component</h1>
   <PersonalInfo />
   <Footer />
