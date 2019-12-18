51a039c21c2a0539d5d7e13a5d3cb5f7823e18eb
Alejandro Salas <asalas.wd@outlook.com>
2019-12-17 23:37:26 -0500

    update App.js location and add all other components there

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './NavBar';
+import PersonalInfo from './PersonalInfo';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <PersonalInfo />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './components/App';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import PersonalInfo from './components/PersonalInfo';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-
-  <div>
-  <NavBar />
-  <h1>Below we have a component</h1>
-  <PersonalInfo />
-  <Footer />
-  </div>,
-  document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
